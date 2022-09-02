f528487563a6059cf7bff7196bca747107019b88
sadkanuos <sounakdas19960@gmail.com>
2022-09-02 19:38:20 +0530

    Adding the "heal" functionality

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function randomValue(min, max){
     return Math.floor(Math.random() *(max - min)) + min;
-}
+};
 Vue.createApp({
     data(){
         return{
@@ -17,7 +17,7 @@ Vue.createApp({
             return { width: this.playerHealth + '%'};
         },
         specialAttackWatcher(){
-            return roundCounter % 4 !== 0;
+            return this.roundCounter % 4 !== 0;
         }
     },
     methods:{
@@ -28,13 +28,23 @@ Vue.createApp({
             this.attackPlayer();    
         },
         attackPlayer(){
-            const attackValue = randomValue(8, 20);
+            const attackValue = randomValue(12, 20);
             this.playerHealth = this.playerHealth - attackValue;
         },
         specialAttackDemon(){
             this.roundCounter++;
-            const attackValue = randomValue(10, 25);
+            const attackValue = randomValue(10, 30);
             this.demonHealth = this.demonHealth - attackValue;
+            this.attackPlayer();
+        },
+        healPlayer(){
+            this.roundCounter++;
+            const healValue = randomValue(12, 20);
+            if (this.playerHealth + healValue > 100){
+                this.playerHealth = 100;
+            } else { 
+                this.playerHealth = this.playerHealth + healValue;
+            }
             this.attackPlayer();
         }
     }
