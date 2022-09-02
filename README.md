2aa84af38fdabf49a41bd3d7b806bd72c768c1a3
sadkanuos <sounakdas19960@gmail.com>
2022-09-02 19:06:50 +0530

    Updating Health Bars

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Vue.js-Project--Demon-Slayer
+# Vue.js Project: Demon-Slayer
 
 This is a project as a part of the UDEMY COURSE: Vue.js is an awesome JavaScript Framework for building Frontend Applications! VueJS mixes the Best of Angular + React!
-This is a turn base health indicator based game as there are no in game assets.
+
--- app.js
+++ app.js
@@ -5,18 +5,26 @@ Vue.createApp({
     data(){
         return{
             playerHealth: 100,
-            monsterHealth: 100
+            demonHealth: 100
         };
     },
+    computed:{
+        demonBarStyles(){
+            return { width: this.demonHealth + '%'};
+        },
+        playerBarStyles(){
+            return { width: this.playerHealth + '%'};
+        }
+    },
     methods:{
-        attackMonster(){
+        attackDemon(){
             const attackValue = randomValue(5, 10);
-            this.monsterHealth = this.monsterHealth - attackValue;
+            this.demonHealth = this.demonHealth - attackValue;
             this.attackPlayer()    
         },
         attackPlayer(){
             const attackValue = randomValue(8, 20);
-            this.playerHealth = this.playerHealth - this.attackValue;
+            this.playerHealth = this.playerHealth - attackValue;
         }
     }
 }).mount('#game');
